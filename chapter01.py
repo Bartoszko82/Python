aa67dd930470a2d2152e6ce40c34543147e05182
Bartoszko <bartek.zawodniak@gmail.com>
2017-06-03 14:11:43 +0200

    intro completed, now lets play with microbit:)

---
+++ chapter01.py
@@ -0,0 +1,71 @@
+#if 2 > 1:
+#    if 3 > 2:
+#        print("OK")
+#    else:
+#        print("FAIL")
+#    print("DONE")
+
+#liczymy bmi
+
+#bmi = int(input())
+#chudy = "niedowaga"
+#normalny = "waga prawidłowa"
+#gruby = "nadwaga"
+#
+#if bmi < 18.5:
+#    print(chudy)
+#if bmi >= 18.5:
+#    if bmi < 25.0:
+#        print(normalny)
+#if bmi >= 25.0:
+#    print(gruby)
+
+#test ELIF
+
+#n = 0
+#
+#if n < 1:
+#    print("jeden")
+#elif n < 2:
+#    print("dwa")
+#elif n < 3:
+#    print("trzy")
+#else:
+#    print("duzo")
+
+#KROTKI
+
+#names = ("Paulina", "Kowalksa")
+#details = (27, 1.70)
+#point = ("Area51", (37, 115))
+#print(names+details)
+#print(point)
+
+#wracamy do BMI + formatowanie 
+
+#bmi = 21.398273497
+bmi = int(input())
+chudy = "niedowaga"
+normalny = "waga prawidłowa"
+gruby = "nadwaga"
+tekst = "bla"
+
+if bmi < 18.5:
+    tekst = chudy
+if bmi >= 18.5:
+    if bmi < 25.0:
+        tekst = normalny
+if bmi >= 25.0:
+    tekst = gruby
+    
+wynikA = "Twoje BMI: %.2f (%s)" % (bmi, tekst)
+wynikB = "Twoje BMI: %.2f (%s)" % (bmi, tekst)
+wynikC = "{:^50}".format("Twoje BMI: %.2f (%s)" % (bmi, tekst))
+wynikD = "Twoje BMI: %.2f (%s)".center(60) % (bmi, tekst)
+
+print("{:^40}".format(wynikA))    
+print(wynikB.center(40, ))
+print(wynikC)
+print(wynikD)
+
+
---
+++ chapter02.py
@@ -0,0 +1,7 @@
+#IF IF IF IF po raz setny
+
+wiek = 23
+if wiek > 20:
+    print("jesteś za stary")
+    print("wiem:(")
+    
